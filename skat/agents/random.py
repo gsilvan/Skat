18476c0f85712a66d5699801010abf89a75828fa
silvan <silvan@trollbox.info>
2022-01-10 15:40:55 +0100

    agents: move VALID_BIDS in abstract Agent to make it available to all future agents

--- skat/agents/random.py
+++ skat/agents/random.py
@@ -12,14 +12,6 @@ class RandomAgent(Agent):
     Random-Agent: An agent that identifies valid moves and chooses on of those
     by an equal distributed random policy.
     """
-    VALID_BIDS = [0, 18, 20, 22, 24, 27, 30, 33, 36, 40, 44, 45, 48, 50, 54,
-                  55,
-                  60, 63, 66, 70, 72, 77, 80, 81, 84, 88, 90, 96, 99, 100, 108,
-                  110, 117, 120, 121, 126, 130, 132, 135, 140, 143, 144, 150,
-                  153, 154, 156, 160, 162, 165, 168, 170, 171, 176, 180, 187,
-                  189, 190, 192, 198, 200, 204, 207, 209, 210, 216, 220, 225,
-                  228, 230, 231, 234, 240, 242, 243, 250, 252, 253, 260, 261,
-                  264]
 
     def __init__(self):
         """Initializes the random agent with a fixed seed."""
