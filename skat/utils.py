8229e68368d42de3c4cf74f5e4f662ecd6097055
silvan <silvan@trollbox.info>
2022-03-10 16:26:56 +0100

    utils: introduce disjoint function

--- skat/utils.py
+++ skat/utils.py
@@ -12,3 +12,14 @@ def to_numpy(hand: list[Card]):
                 arr[i] = True
             i += 1
     return arr
+
+
+def disjoint(items):
+    """Return True if the flatten set of list items is disjoint."""
+    union = set()
+    for item in items:
+        for x in item:
+            if x in union:
+                return False
+            union.add(x)
+    return True
---
+++ tests/test_utils.py
@@ -0,0 +1,40 @@
+import unittest
+
+from skat.card import Card
+from skat.utils import disjoint
+
+
+class UtilsTest(unittest.TestCase):
+    def test_disjoint(self):
+        simple_list = [
+            (
+                1,
+                2,
+                3,
+            ),
+            (4, 5),
+            (42, 23),
+        ]
+        self.assertTrue(disjoint(simple_list))
+        simple_list.append((3, 0))
+        self.assertFalse(disjoint(simple_list))
+        card_list = [
+            (
+                Card(0, 0),
+                Card(3, 5),
+                Card(2, 1),
+            ),
+            (
+                Card(1, 0),
+                Card(1, 5),
+                Card(1, 1),
+            ),
+            (
+                Card(2, 0),
+                Card(2, 5),
+                Card(2, 2),
+            ),
+        ]
+        self.assertTrue(disjoint(card_list))
+        card_list.append((Card(1, 5), Card(1, 2)))
+        self.assertFalse(disjoint(card_list))
