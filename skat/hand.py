052e3e88a59be92bc50331e82d2d70082cdb0868
silvan <silvan@trollbox.info>
2022-03-23 08:45:53 +0100

    hand: sort() will sort the hand according to a given order

--- skat/hand.py
+++ skat/hand.py
@@ -1,5 +1,6 @@
 import collections.abc
 import typing
+from collections import namedtuple
 
 import numpy as np
 
@@ -7,6 +8,12 @@ from skat.card import Card
 
 
 class Hand(collections.abc.MutableSequence):
+    Order = namedtuple(
+        "Order",
+        ("suits", "ranks", "pivot_suits", "pivot_ranks"),
+        defaults=("", "", "♦♥♠♣", ""),
+    )
+
     def __init__(self, iterable=None) -> None:
         self.__hand: list[Card] = list()
         if iterable:
@@ -36,6 +43,9 @@ class Hand(collections.abc.MutableSequence):
     def __repr__(self):
         return self.__str__()
 
+    def __eq__(self, other):
+        return self.__hand == other.__hand
+
     def append(self, card: Card) -> None:
         self.__hand.append(card)
 
@@ -56,3 +66,34 @@ class Hand(collections.abc.MutableSequence):
         for card in self.__hand:
             arr[card.np_index] = 1
         return arr
+
+    def sort(self, order: Order):
+        """Sort the Hand according to specified sort rules using Bubble-Sort."""
+        while True:
+            has_changed = False
+            for i in range(len(self.__hand) - 1):
+                if not self.tgt(self.__hand[i], self.__hand[i + 1], order):
+                    pivot = self.__hand[i]
+                    self.__hand[i] = self.__hand[i + 1]
+                    self.__hand[i + 1] = pivot
+                    has_changed = True
+            if not has_changed:
+                return
+
+    @staticmethod
+    def tgt(a: Card, b: Card, order: Order):
+        """Test if card `a` is better than card `b`"""
+        pivot_chars = tuple(order.pivot_ranks)
+        pivot_suits = tuple(order.pivot_suits)
+        if a.rank in pivot_chars or b.rank in pivot_chars:
+            if a.rank == b.rank:
+                return pivot_suits.index(a.suit) > pivot_suits.index(b.suit)
+            if a.rank in pivot_chars and b.rank in pivot_chars:
+                return pivot_chars.index(a.rank) > pivot_chars.index(b.rank)
+            else:
+                return a.rank in pivot_chars
+        if a.suit == b.suit:
+            return tuple(order.ranks).index(a.rank) > tuple(order.ranks).index(b.rank)
+        if not a.suit == b.suit:
+            return tuple(order.suits).index(a.suit) > tuple(order.suits).index(b.suit)
+        raise Exception("Something went completely wrong!")
--- tests/test_hand.py
+++ tests/test_hand.py
@@ -88,3 +88,54 @@ class HandTest(unittest.TestCase):
             ]
         )
         self.assertTrue(np.array_equal(arr, self.filled_hand.as_vector))
+
+    def test_tgt(self) -> None:
+        diamonds_order = Hand.Order(
+            suits="♦♠♣♥", ranks="789QKXA", pivot_suits="♦♥♠♣", pivot_ranks="J"
+        )
+
+        self.assertTrue(Hand.tgt(Card(0, 1), Card(0, 0), diamonds_order))
+        self.assertFalse(Hand.tgt(Card(0, 0), Card(0, 1), diamonds_order))
+
+        self.assertTrue(Hand.tgt(Card(0, 6), Card(0, 0), diamonds_order))
+        self.assertFalse(Hand.tgt(Card(0, 0), Card(0, 6), diamonds_order))
+
+        self.assertTrue(Hand.tgt(Card(3, 3), Card(0, 3), diamonds_order))
+        self.assertFalse(Hand.tgt(Card(0, 3), Card(3, 3), diamonds_order))
+
+        for i in range(4):
+            a = Card(i, 3)
+            b = Card(0, 7)
+            self.assertTrue(Hand.tgt(a, b, diamonds_order))
+            self.assertFalse(Hand.tgt(b, a, diamonds_order))
+
+        # Sheep head prep :)
+
+        sheep_head = Hand.Order(
+            suits="♦♥♣♠", ranks="789KXA", pivot_suits="♦♥♠♣", pivot_ranks="JQ"
+        )
+
+        self.assertTrue(Hand.tgt(Card(2, 0), Card(3, 0), sheep_head))
+        self.assertFalse(Hand.tgt(Card(3, 0), Card(2, 0), sheep_head))
+
+        for i in range(4):
+            a = Card(i, 4)
+            b = Card(i, 3)
+            self.assertTrue(Hand.tgt(a, b, sheep_head))
+            self.assertFalse(Hand.tgt(b, a, sheep_head))
+
+    def test_eq(self) -> None:
+        self.assertEqual(Hand([Card(0, 2), Card(1, 1)]), Hand([Card(0, 2), Card(1, 1)]))
+
+    def test_sort(self) -> None:
+        hearts_order = Hand.Order(
+            suits="♦♠♣♥", ranks="789QKXA", pivot_suits="♦♥♠♣", pivot_ranks="J"
+        )
+        initial_hand = Hand(
+            [Card(2, 2), Card(3, 3), Card(0, 3), Card(1, 2), Card(3, 4)]
+        )
+        initial_hand.sort(hearts_order)
+        expected_hand = Hand(
+            [Card(3, 3), Card(0, 3), Card(1, 2), Card(3, 4), Card(2, 2)]
+        )
+        self.assertEqual(expected_hand, initial_hand)
