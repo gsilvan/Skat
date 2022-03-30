08a0532ef8d4236d95f3021ad15549bc809649dc
silvan <silvan@trollbox.info>
2022-03-30 10:52:52 +0200

    model: remove redundant forward pass specification using torch.nn.Sequential

--- skat/models/suit_solo.py
+++ skat/models/suit_solo.py
@@ -1,5 +1,4 @@
 import torch.nn as nn
-import torch.nn.functional as F
 
 
 class SuitSoloNet(nn.Module):
@@ -17,27 +16,22 @@ class SuitSoloNet(nn.Module):
         self,
         input_size=INPUT_SIZE,
         output_size=OUTPUT_SIZE,
-        hidden_activation=F.relu,
-        output_activation=F.softmax,
         dropout=0.25,
     ) -> None:
         super().__init__()
-        self.activation = hidden_activation
-        self.output_activation = output_activation
-        self.input = nn.Linear(in_features=input_size, out_features=output_size)
-        self.fc1 = nn.Linear(input_size, 128)
-        self.fc2 = nn.Linear(128, 64)
-        self.fc3 = nn.Linear(64, 32)
-        self.drop_p = dropout
+        self.linear_dropout_relu_stack = nn.Sequential(
+            nn.Linear(in_features=input_size, out_features=input_size),
+            nn.Dropout(p=dropout),
+            nn.ReLU(),
+            nn.Linear(input_size, 128),
+            nn.Dropout(p=dropout),
+            nn.ReLU(),
+            nn.Linear(128, 64),
+            nn.Dropout(p=dropout),
+            nn.ReLU(),
+            nn.Linear(64, output_size),
+        )
 
     def forward(self, x):
-        x = self.input(x)
-        x = self.fc1(x)
-        x = F.dropout(x, self.drop_p)
-        x = self.activation(x)
-        x = self.fc2(x)
-        x = F.dropout(x, self.drop_p)
-        x = self.activation(x)
-        x = self.fc3(x)
-        x = self.output_activation(x, -1)
+        x = self.linear_dropout_relu_stack(x)
         return x
