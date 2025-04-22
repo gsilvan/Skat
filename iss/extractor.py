1fa11ec7637b3fe4c73c0e5662992149c7a9045b
Silvan Gümüsdere <silvan@trollbox.org>
2025-04-22 14:05:55 +0200

    make it portable
    Usage example: `python -m iss.extractor ~/Downloads/iss-games-07-2024.sgf`

--- iss/extractor.py
+++ iss/extractor.py
@@ -1,31 +1,42 @@
 #!/usr/bin/env python3
+import sys
+import os.path
+
 from iss import GameType, ISSGames
 
-if __name__ == "__main__":
-    suit_games = []
-    grand_games = []
-    null_games = []
 
-    iss_games = ISSGames("/home/silvan/Downloads/iss-games-04-2021.sgf")
-    full_sample = iss_games.sample(n=len(iss_games))
-    for game in full_sample:
-        if not game.is_valid:
-            continue
-        # if not game.is_won:
-        #     continue
-        match game.type:
-            case GameType.DIAMONDS | GameType.HEARTS | GameType.SPADES | GameType.CLUBS:
-                suit_games.append(game)
-            case GameType.GRAND:
-                grand_games.append(game)
-            case GameType.NULL:
-                null_games.append(game)
-    for game in suit_games:
-        with open("/home/silvan/iss_games/suit_win_loss.sgf", "a") as file:
-            file.write(f"{str(game)}\n")
-    for game in grand_games:
-        with open("/home/silvan/iss_games/grand_win_loss.sgf", "a") as file:
-            file.write(f"{str(game)}\n")
-    for game in null_games:
-        with open("/home/silvan/iss_games/null_win_loss.sgf", "a") as file:
-            file.write(f"{str(game)}\n")
+if len(sys.argv) < 1:
+    print("python extractor.py <path-to-sgf-file>")
+    exit()
+sgf_file = sys.argv[1]
+
+full_path = os.path.abspath(sgf_file)
+directory = os.path.dirname(sgf_file)
+
+suit_games = []
+grand_games = []
+null_games = []
+
+iss_games = ISSGames(full_path)
+full_sample = iss_games.sample(n=len(iss_games))
+for game in full_sample:
+    if not game.is_valid:
+        continue
+    # if not game.is_won:
+    #     continue
+    match game.type:
+        case GameType.DIAMONDS | GameType.HEARTS | GameType.SPADES | GameType.CLUBS:
+            suit_games.append(game)
+        case GameType.GRAND:
+            grand_games.append(game)
+        case GameType.NULL:
+            null_games.append(game)
+for game in suit_games:
+    with open(os.path.join(directory, "suit_win_loss.sgf"), "a") as file:
+        file.write(f"{str(game)}\n")
+for game in grand_games:
+    with open(os.path.join(directory, "grand_win_loss.sgf"), "a") as file:
+        file.write(f"{str(game)}\n")
+for game in null_games:
+    with open(os.path.join(directory, "null_win_loss.sgf"), "a") as file:
+        file.write(f"{str(game)}\n")
